0c2f4a4a0662e42823a874ce5eeb7afbd252dc55
Pablo Becerra <pablo_becerra94@yahoo.com.ar>
2016-09-07 18:33:40 -0300

    Forma fea
    Agregado de forma fea para el constructor por archivos del SEL

--- src/selMath/SistemasDeEcuacionesLineales.java
+++ src/selMath/SistemasDeEcuacionesLineales.java
@@ -1,6 +1,8 @@
 package selMath;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SistemasDeEcuacionesLineales {
 	private MatrizMath matriz;
@@ -18,10 +20,31 @@ public class SistemasDeEcuacionesLineales {
 	}
 	
 	public SistemasDeEcuacionesLineales(String path) throws FileNotFoundException{
-		matriz= new MatrizMath(path);
+	    matriz= new MatrizMath(path);
 		resultado=new VectorMath(path);
-		//FIXME buscar la forma de que esto ande asi para reutilizar codigo.
+		//FIXME buscar la forma de que esto ande asi para reutilizar codigo. abajo dejo la forma fea
+    /* 
+		Scanner sc = new Scanner(new File(path));
+		matriz.setFila(sc.nextInt());
+		matriz.setColumna(sc.nextInt());
+		matriz = new MatrizMath(matriz.getFila(),matriz.getColumna());
 		
+		double[][] matrizCargadora = new double[matriz.getFila()][matriz.getColumna()];
+
+		for(int i=0; i<matriz.getFila()*matriz.getColumna();i++){
+			matrizCargadora[sc.nextInt()][sc.nextInt()] = sc.nextDouble();
+		}
+		
+		 matriz.setMatriz(matrizCargadora);
+		 
+		 
+		 double[] vector = new double[sc.nextInt()];
+			for(int i=0;i<vector.length;i++){
+				vector[i] = sc.nextDouble();
+			}
+			resultado.setVector(vector);
+		 sc.close();
+		*/
 	}
 	
 
